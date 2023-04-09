b3c99cb1140eea3f137fcad3450a2e6d6b94257a
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-09 19:10:14 -0300

    desenvolvido o componente menu para alternar entre paginas e destacar qual pagina esta sendo exibida

---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './Menu.module.css'
+import MenuLink from '../MenuLink/index.js'
+
+export default function Menu() {
+    
+    
+  
+    return (
+    <header>
+        <nav className={styles.navegacao}>
+            <MenuLink to='/'>
+                Inicio
+            </MenuLink>
+            <MenuLink to='/SobreMim'>
+                Sobre mim
+            </MenuLink>
+        </nav>
+    </header>
+  )
+}
