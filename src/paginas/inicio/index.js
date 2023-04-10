b122ab89efbdd84b67a88b5ef13905d4737563db
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-09 23:32:38 -0300

    seção post desenvolvido e implementada

--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Banner from 'componentes/Banner/index.js'
+import styles from './inicio.module.css'
+import posts from 'json/posts.json'
+import Post from 'componentes/Post'
 
 export default function Inicio() {
   return (
@@ -7,7 +10,13 @@ export default function Inicio() {
     <main>
       <Banner />
 
-      <h1>Ola mundo!</h1>
+      <ul className={styles.posts}>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Post post={post} />            
+          </li>
+        ))}
+      </ul> 
     </main>
   )
 }
