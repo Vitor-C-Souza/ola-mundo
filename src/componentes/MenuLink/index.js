4677e7a8d72e47eb5a1ddf5960094f6beb4fa141
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-09 21:01:52 -0300

    alterado a tag Link para navlink para não depender de usar userLocation

--- src/componentes/MenuLink/index.js
+++ src/componentes/MenuLink/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import styles from "./MenuLink.module.css";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-export default function MenuLink({children, to}) {
-    const localizacao = useLocation()
-  
-    return (
-    <Link
-      className={`
-                ${styles.link}
-                ${localizacao.pathname === to ? styles.linkDestacado : ""}
-            `}to={to}>
+export default function MenuLink({ children, to }) {
+  return (
+    <NavLink
+      className={({ isActive }) => `
+        ${styles.link}
+        ${isActive ? styles.linkDestacado : ""}
+        
+      `}
+        to={to}
+        end
+    >
       {children}
-    </Link>
+    </NavLink>
   );
 }
