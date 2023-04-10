b4e2a2e058c2059ada926a20753cf9a28d9771b7
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-09 23:37:25 -0300

    Desenvolvido rodape

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './paginas/inicio'
 import SobreMim from './paginas/SobreMim'
 import Menu from './componentes/Menu/index.js';
+import Rodape from 'componentes/Rodape/index.js';
 
 
 
@@ -14,6 +15,8 @@ function AppRoutes() {
         <Route path='/SobreMim' element={<SobreMim />} />
         <Route path='*' element={<div>Pagina não encontrada</div>} />
       </Routes>
+
+      <Rodape />
     </BrowserRouter>
   )
 }
