77ce6124866c6e131904f4ecb44fb7d14bdbe601
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-11 18:02:53 -0300

    implementado a pagina não encontrado na pagina do post se nenhum post for encontrado

--- src/componentes/Post/Index.js
+++ src/componentes/Post/Index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 import posts from "json/posts.json";
 import PostModelo from "componentes/PostModelo/index.js";
 import ReactMarkdown from "react-markdown";
 import "./Post.css";
+import NaoEncontrada from "paginas/NaoEncontrado/Index.js";
+import PaginaPadrao from "componentes/paginaPadrao/index.js";
 
 export default function Post() {
   const parametros = useParams();
@@ -13,16 +15,25 @@ export default function Post() {
   });
 
   if (!post) {
-    return <h1>Post não encontrado...</h1>;
+    return <NaoEncontrada />;
   }
   return (
-    <PostModelo
-      fotoCapa={`/assets/posts/${post.id}/capa.png`}
-      titulo={post.titulo}
-    >
-      <div className="post-markdown-container">
-        <ReactMarkdown>{post.texto}</ReactMarkdown>
-      </div>
-    </PostModelo>
+    <Routes>
+      <Route path="*" element={<PaginaPadrao />}>
+        <Route
+          index
+          element={
+            <PostModelo
+              fotoCapa={`/assets/posts/${post.id}/capa.png`}
+              titulo={post.titulo}
+            >
+              <div className="post-markdown-container">
+                <ReactMarkdown>{post.texto}</ReactMarkdown>
+              </div>
+            </PostModelo>
+          }
+        ></Route>
+      </Route>
+    </Routes>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Inicio from './paginas/inicio'
-import SobreMim from './paginas/SobreMim'
-import Menu from './componentes/Menu/index.js';
-import Rodape from 'componentes/Rodape/index.js';
-import PaginaPadrao from 'componentes/paginaPadrao/index.js';
-import Post from 'componentes/Post/Index.js';
-import NaoEncontrado from 'paginas/NaoEncontrado/Index.js';
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/SobreMim";
+import Menu from "./componentes/Menu/index.js";
+import Rodape from "componentes/Rodape/index.js";
+import PaginaPadrao from "componentes/paginaPadrao/index.js";
+import Post from "componentes/Post/Index.js";
+import NaoEncontrado from "paginas/NaoEncontrado/Index.js";
 
 function AppRoutes() {
   return (
@@ -15,17 +13,17 @@ function AppRoutes() {
       <Menu />
 
       <Routes>
-        <Route path='/' element={<PaginaPadrao />}>
-          <Route index element={<Inicio />}/>
-          <Route path='SobreMim' element={<SobreMim />} />
-          <Route path='posts/:id' element={<Post />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path="SobreMim" element={<SobreMim />} />
         </Route>
-        <Route path='*' element={<NaoEncontrado />} />
+        <Route path="posts/:id/*" element={<Post />} />
+        <Route path="*" element={<NaoEncontrado />} />
       </Routes>
 
       <Rodape />
     </BrowserRouter>
-  )
+  );
 }
 
 export default AppRoutes;
